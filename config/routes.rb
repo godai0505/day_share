a0df98e7c8c00714902185e2f1055f7665421996
godai0505 <tanagoda.tech@gmail.com>
2018-03-17 19:49:29 +0000

    "create html and css for top-page"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/', to: 'index#index'
+  get 'sign_in', to: 'index#sign_in'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
 end
