a9bfa0c8da11680cee6801a7b4de9d31f4837c73
godai0505 <tanagoda.tech@gmail.com>
2018-04-01 06:34:11 +0000

    重複していたsigninページを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get '/', to: 'index#index'
-  get 'sign_in', to: 'index#sign_in'
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
