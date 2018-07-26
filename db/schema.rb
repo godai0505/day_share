e0e58d7af7a1a6668817a45d03e06b2578103237
godai0505 <tanagoda.tech@gmail.com>
2018-07-26 14:27:35 +0000

    userにnameを追加&strong_parameterにも追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180701051138) do
+ActiveRecord::Schema.define(version: 20180725151858) do
 
   create_table "bookmark_details", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "bookmark_id"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20180701051138) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
