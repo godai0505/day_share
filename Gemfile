195ca654fb10adafab75898d20e6c3d856fd4dc6
godai0505 <tanagoda.tech@gmail.com>
2018-07-26 15:49:40 +0000

    rspecの導入

--- Gemfile
+++ Gemfile
@@ -23,6 +23,9 @@ gem 'uglifier', '>= 1.3.0'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'rails-controller-testing'
   gem 'capybara', '~> 2.13'
   gem 'pry-byebug'
   gem 'pry-doc'
--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,22 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module DayShare
+module Sociab
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
+    config.generators do |g|
+      # 色々な記述があるので、一番下に追記する
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
