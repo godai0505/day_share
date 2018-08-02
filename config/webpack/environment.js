b90075e1fdfd38bf38b354a68f3740c590db586d
godai0505 <tanagoda.tech@gmail.com>
2018-08-02 15:51:42 +0000

    vue.jsの導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,10 @@
 const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
+const webpack = require('webpack')
 
 environment.loaders.append('vue', vue)
-module.exports = environment
+environment.plugins.set('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
